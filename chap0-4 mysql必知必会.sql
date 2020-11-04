f5a41b804c321111f15296d24b22d0eda3219bc2
Sai <45934325+saisadomi@users.noreply.github.com>
2020-11-04 20:37:15 +0800

    Add files via upload
    Mysql必知必会前四张内容

---
+++ chap0-4 mysql必知必会.sql
@@ -0,0 +1,2 @@
+INSERT INTO `<table_name>`(`runoob_id`, `runoob_title`, `runoob_author`, `submission_date`) VALUES (1, '学习PHP', '菜鸟教程', '2020-11-04');
+INSERT INTO `<table_name>`(`runoob_id`, `runoob_title`, `runoob_author`, `submission_date`) VALUES (2, '学习Mysql', '菜鸟教程', '2020-11-04');
